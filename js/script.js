73bb2f8b1a51dba0b435019a39ab16596e124bd5
Product0wner <jaanbrunken@gmail.com>
2020-02-27 21:46:34 +0100

    final changes
    added individual messages

--- js/script.js
+++ js/script.js
@@ -135,12 +135,15 @@ $("#mail").after('<p><span class="errorMessage" id="email_error_format">Please e
 $("#cc-num").after('<p><span class ="errorMessage "id="cc-num_error">Please use a valid card (13-16 digits) </span></p>')
 $("#zip").after('<p><span class="errorMessage" id="zip_error">Zip needs 5 digits </span></p>')
 $("#cvv").after('<p><span class="errorMessage" id="cvv_error">CVV needs 3 digits </span></p>')
+$(".activities").after('<p><span class="errorMessage" id="activity_error"> You need to select at least 1 activity </span></p>')
 
 $('#name').on('keypress keydown keyup',function(){
     if(!$(this).val().match($userNameRGEX)) {
         $('#name_error').show();
+        $("#name").css("border-bottom-color", "red")
     } else {
         $('#name_error').hide();
+        $("#name").css("border-bottom-color", "green")
     }
 });
 
@@ -148,77 +151,128 @@ $('#mail').on('keypress keydown keyup',function(){
     if ((!$(this).val().match($userNameRGEX)) && !$(this).val().match($userEmailRGEX)) {
         $('#email_error_format').hide();
         $('#email_error_empty').show();
+        $("#mail").css("border-bottom-color", "red")
     } else if (!$(this).val().match($userEmailRGEX)) {
         $('#email_error_empty').hide();
         $('#email_error_format').show();
+        $("#mail").css("border-bottom-color", "red")
     } else {
         $('#email_error_format').hide();
         $('#email_error_empty').hide();
+        $("#mail").css("border-bottom-color", "green")
     }
 });
 
 $('#cc-num').on('keypress keydown keyup',function(){
     if (!$(this).val().match($creditCardRGEX)) {
         $('#cc-num_error').show();
+        $("#cc-num").css("border-bottom-color", "red")
     } else {
         $('#cc-num_error').hide();
+        $("#cc-num").css("border-bottom-color", "green")
     }
 });
 
 $('#zip').on('keypress keydown keyup',function(){
     if (!$(this).val().match($zipCodeRGEX)) {
         $('#zip_error').show();
+        $("#zip").css("border-bottom-color", "red");
     } else {
         $('#zip_error').hide();
+        $("#zip").css("border-bottom-color", "green");
     }
 });
 $('#cvv').on('keypress keydown keyup',function(){
     if (!$(this).val().match($cvvCodeRGEX)) {
         $('#cvv_error').show();
+        $("#cvv").css("border-bottom-color", "red");
     } else {
         $('#cvv_error').hide();
+        $("#cvv").css("border-bottom-color", "green");
     }
 });
 
-// validation that checks inputs and only submits form when inputs are correct
-let validateFormTest = () => {
+let nameValidation = () => {
     if(!$("#name").val().match($userNameRGEX)) {
-        alert("Upps, seems like there is a problem with your username");
+        $("#name").css("border-bottom-color", "red")
         $("#name").first().focus();
         $('#name_error').show();
         return false
-    } else if(!$("#mail").val().match($userEmailRGEX) && !$("#mail").val().match($userNameRGEX)) {
-        alert("Dont leave your email blank.");
+    } 
+}
+
+let emailBlancValidation = () => {
+    if(!$("#mail").val().match($userEmailRGEX) && !$("#mail").val().match($userNameRGEX)) {
+        $("#mail").css("border-bottom-color", "red");
         $("#mail").first().focus();
         $('#email_error_empty').show();
         return false
-    } else if(!$("#mail").val().match($userEmailRGEX)) {
-        alert("Duh, your email seems not valid.");
+    } 
+}
+
+let emailFormValidation = () => {
+    if(!$("#mail").val().match($userEmailRGEX)) {
+        $("#mail").css("border-bottom-color", "red");
         $("#mail").first().focus();
         $('#email_error_format').show();
         return false
-    } else if (selectedActivties < 1){
-        alert("You need to select at least 1 activity")
+    } 
+}
+
+let activityValidator = () => {
+    if (selectedActivties < 1){
+        $("#activity_error").css("color", "red");
+        $("#activity_error").show();
         return false
-    } else if( $('#payment').val() === "credit card" && !$("#cc-num").val().match($creditCardRGEX)) {
-        alert("Please check your credit card number.");
+    } 
+}
+
+let ccNumValidator = () => {
+    if( $('#payment').val() === "credit card" && !$("#cc-num").val().match($creditCardRGEX)) {
+        $("#cc-num").css("border-bottom-color", "red");
         $("#cc-num").first().focus();
         $('#cc-num_error').show();
         return false
-    } else if($('#payment').val() === "credit card" && !$("#zip").val().match($zipCodeRGEX)) {
-        alert("Sorry, the Zip code needs to have 5 digits");
+    }
+}
+
+let zipValidator = () => {
+    if($('#payment').val() === "credit card" && !$("#zip").val().match($zipCodeRGEX)) {
+        $("#zip").css("border-bottom-color", "red");
         $("#zip").first().focus();
         $('#zip_error').show();
         return false
-    } else if($('#payment').val() === "credit card" && !$("#cvv").val().match($cvvCodeRGEX)) {
+    } 
+}
+
+let cvvValidator = () => {
+    if($('#payment').val() === "credit card" && !$("#cvv").val().match($cvvCodeRGEX)) {
         alert("Nope, not gonna work, your CVV is invalid");
+        $("#cvv").css("border-bottom-color", "red");
         $("#cvv").first().focus();
         $('#cvv_error').show();
         return false
-    } else if (selectedActivties < 1){
-        alert("You need to select at least 1 activity")
+    } 
+}
+
+// validation that checks inputs and only submits form when inputs are correct
+let validateFormTest = (e) => {
+    if(nameValidation()) {
         return false
-    }
+    } else if(emailFormValidation()) {
+        e.preventDefault();
+    } else if (activityValidator()){
+        e.preventDefault();
+    } else if(ccNumValidator()) {
+        e.preventDefault();
+    } else if(zipValidator()) {
+        e.preventDefault();
+    } else if($('#payment').val() === "credit card" && !$("#cvv").val().match($cvvCodeRGEX)) {
+        $("#cvv").css("border-bottom-color", "red");
+        $("#cvv").first().focus();
+        $('#cvv_error').show();
+        return false
+    } else {return true}
 }
     
 
